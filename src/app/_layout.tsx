4c8a4d0e5dbc327541d84758cd9688e5f4996a9e
sumanta ghosh <84187999+sumantaGhosh24@users.noreply.github.com>
2026-05-20 12:58:23 +0530

    added icons & dark mode

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,15 @@
+import {useColorScheme} from "react-native";
 import {Stack} from "expo-router";
+import {DarkTheme, DefaultTheme, ThemeProvider} from "@react-navigation/native";
 
 import "../../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack />
+    </ThemeProvider>
+  );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,75 @@
-import {Text, View} from "react-native";
+import {useColorScheme} from "nativewind";
+import {useCallback} from "react";
+import {Text, TouchableOpacity, View} from "react-native";
+import {Ionicons} from "@expo/vector-icons";
 
 export default function Index() {
+  const {colorScheme, toggleColorScheme} = useColorScheme();
+
+  const handleToggleColorScheme = useCallback(() => {
+    toggleColorScheme();
+  }, [toggleColorScheme]);
+
   return (
-    <View className="flex-1 items-center justify-center bg-blue-50">
+    <View className="flex-1 items-center justify-center bg-blue-50 dark:bg-black">
       <Text className="text-2xl font-bold mb-2 text-primary">
         Welcome to AI Byte Expo!
       </Text>
       <Text className="text-base text-gray-600">
         This is a dummy index screen using NativeWind.
       </Text>
+      <View>
+        <TouchableOpacity
+          className="flex-row items-center p-4 rounded-lg mb-2"
+          style={{
+            backgroundColor: colorScheme === "light" ? "#e5e5e5" : "#1f2937",
+          }}
+          disabled={colorScheme === "light"}
+          onPress={handleToggleColorScheme}
+        >
+          <Ionicons
+            name="sunny"
+            size={24}
+            color={colorScheme === "dark" ? "white" : "black"}
+          />
+          <Text className="text-lg ml-3 dark:text-white">
+            Light {colorScheme === "light" && "(Active)"}
+          </Text>
+          {colorScheme === "light" && (
+            <Ionicons
+              name="checkmark-circle"
+              size={20}
+              color="#155dfc"
+              style={{marginLeft: "auto"}}
+            />
+          )}
+        </TouchableOpacity>
+        <TouchableOpacity
+          className="flex-row items-center p-4 rounded-lg mb-2"
+          style={{
+            backgroundColor: colorScheme === "dark" ? "#1f2937" : "#e5e5e5",
+          }}
+          disabled={colorScheme === "dark"}
+          onPress={handleToggleColorScheme}
+        >
+          <Ionicons
+            name="moon"
+            size={24}
+            color={colorScheme === "dark" ? "white" : "black"}
+          />
+          <Text className="text-lg ml-3 dark:text-white">
+            Dark {colorScheme === "dark" && "(Active)"}
+          </Text>
+          {colorScheme === "dark" && (
+            <Ionicons
+              name="checkmark-circle"
+              size={20}
+              color="#155dfc"
+              style={{marginLeft: "auto"}}
+            />
+          )}
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
